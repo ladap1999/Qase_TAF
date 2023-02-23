3e54ef7c63f4eab607f4d78c41d6ae8092e531a1
SMeleshk <svetla.meleshko@gmail.com>
2023-02-23 23:06:09 +0300

    Login Page

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import baseEntities.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasePage {
+    private final By emailInputLocator = By.id("inputEmail");
+    private final By passwordInputLocator = By.id("inputPassword");
+    private final By logInButtonLocator = By.id("btnLogin");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected By getPageIdentifier() {
+        return logInButtonLocator;
+    }
+
+    public By getLogInButtonLocator() {
+        return logInButtonLocator;
+    }
+
+    public WebElement getEmailInput() { return driver.findElement(emailInputLocator);}
+    public WebElement getPasswordInput() { return driver.findElement(passwordInputLocator);}
+    public WebElement getLoginButton() { return driver.findElement(logInButtonLocator);}
+
+    public void login(String email, String password) {
+        getEmailInput().sendKeys(email);
+        getPasswordInput().sendKeys(password);
+        getLoginButton().click();
+    }
+}
