aade260006337c6be99af83d812a71ae9aa66249
Lada Petkevich <ladap1999@gmail.com>
2023-03-03 22:17:55 +0300

    add checking pop up title in suite

---
+++ src/test/java/stepDefs/SuiteStepDefs.java
@@ -0,0 +1,24 @@
+package stepDefs;
+
+import baseEntities.BaseCucumberTest;
+import io.cucumber.java.en.Then;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import pages.ProjectPage;
+
+public class SuiteStepDefs extends BaseCucumberTest {
+    private ProjectPage projectPage;
+    Logger logger = LogManager.getLogger(ProjectApiStepDefs.class);
+    Logger loggerFile = LogManager.getLogger("File");
+
+    @Then("suite with {string} is presented")
+    public void isSuitePresented(String suiteTitle) {
+        logger.info("Find suite with title " + suiteTitle);
+        loggerFile.info("Find suite with title " + suiteTitle);
+
+        projectPage = new ProjectPage(driver);
+
+        Assert.assertEquals(projectPage.getSuiteTitle().getText(), suiteTitle);
+    }
+}
